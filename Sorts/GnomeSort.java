e34b460182f84f65f88aa7bcdfb51b84d8d70c98
duchao <duchao@baoyinxiaofei.com>
2019-07-09 16:22:56 +0800

    Dc,注释(gnome)

--- Sorts/GnomeSort.java
+++ Sorts/GnomeSort.java
@@ -8,6 +8,15 @@ import static Sorts.SortUtils.*;
  * @author Podshivalov Nikita (https://github.com/nikitap492)
  * @since 2018-04-10
  **/
+
+/**
+ * @author Dc
+ * 地精排序
+ * 当标志有效且 i >(<) i+1 时交换，否则不交换
+ * 只要交换   i = i - 1
+ * 只要补交换 i = i + 1
+ *
+ */
 public class GnomeSort implements SortAlgorithm {
 
     @Override
@@ -15,16 +24,26 @@ public class GnomeSort implements SortAlgorithm {
         int i = 1;
         int j = 2;
         while (i < arr.length) {
-            if (less(arr[i - 1], arr[i])) i = j++;
+            if (less(arr[i - 1], arr[i]))
+                i = j++;
             else {
                 swap(arr, i - 1, i);
-                if (--i == 0) {
+                if (--i == 0)
                     i = j++;
-                }
             }
         }
 
-        return null;
+
+        return arr;
+    }
+
+    private void gnome(Integer[] array) {
+        int i = -1;
+        while (i < array.length - 1) {
+            if (i != -1 && array[i] > array[i + 1])
+                swap(array,i + 1,i --);
+            else i ++;
+        }
     }
 
     public static void main(String[] args) {
@@ -32,7 +51,9 @@ public class GnomeSort implements SortAlgorithm {
         String[] strings = {"c", "a", "e", "b", "d", "dd", "da", "zz", "AA", "aa", "aB", "Hb", "Z"};
         GnomeSort gnomeSort = new GnomeSort();
 
-        gnomeSort.sort(integers);
+//        gnomeSort.sort(integers);
+        gnomeSort.gnome(integers);
+
         gnomeSort.sort(strings);
 
         System.out.println("After sort : ");
